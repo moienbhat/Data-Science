4663d45f01e4907f5cee7ad1f3ffc8f47e2cb059
Moien Bhat <bhatmoien34@gmail.com>
2026-04-04 22:08:17 +0530

    Add files via upload

---
+++ Python Basics/code.py
@@ -0,0 +1,26 @@
+print("Hello World")
+
+name = "moien"
+age = 21
+
+print("My name is", name)
+
+print(type(age))
+
+x = 5
+isPrime = True
+
+y = None
+
+
+tot_price = 100.    # snake case
+
+a = input("Enter a: ")
+b = input("Enter b: ")
+
+sum = int (a) + int (b) 
+print ("the sum is", sum)
+
+
+avg = (int (a) + int(b)) / 2
+print("the average is: ", avg)
