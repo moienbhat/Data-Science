db6d1c7d968cbc4e9eeb7c37c811664f0bdb14fe
Moien Bhat <bhatmoien34@gmail.com>
2026-04-04 22:09:50 +0530

    Add files via upload

---
+++ Python Basics/conditionals.py
@@ -0,0 +1,22 @@
+for i in range(1, 20):
+    print(i)
+
+n = int(input("Enter the number : "))
+
+if(n%2 == 0):
+    print("Even")
+else:
+    print("Odd")
+
+while i>=1:
+    print(i)
+    i-=1
+
+m = int(input("Enter the number : "))
+
+for i in range(1, 10):
+    x = m*i
+    print(f"{m} * {i} = {x}")
+
+
+
